6c9e1536fa2adba6436f49b260735814cdf167db
Maria Garcia <licgarcia_psic@hotmail.com>
2023-05-09 10:40:03 -0600

    ruta absoluta true or false

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-module.exports = () => {
-  // ...
+// funcion llamada file exists, donde probamos si el path existe
+import { existsSync } from "fs";
+import { isAbsolute, resolve } from "path";
+
+
+const mdLinks = (path,  options) => {
+  //sintaxis nueva promesa
+  // resolve y reject son callbacks, para usar posteriormente en las promesas
+  return new Promise((resolve, rejects) => {
+    //identifica si la ruta existe
+    if (existsSync(path)) {
+      //checar o convertir a un ruta absoluta (si es un archivo o directorio)
+    } else {
+      //Si  no existe la ruta, rechaza la promesa
+      rejects("Esta ruta no existe");
+    }
+  });
 };
+  
+const isPathAbsolute =isAbsolute("README.md");
+console.log(isPathAbsolute, '******');
+if(isPathAbsolute === true){
+  console.log("ruta absoluta")
+}else {
+  console.log (resolve('README.md'), 'README.md')
+  //convertir la ruta
+}
+   mdLinks();
+
+
+/*export default {
+  mdLinks,
+  path,
+};*/
+
+
+
+
+/*import { mdLinks} from '../mdlinks.js'
+
+
+describe ('mdLinks', () => {
+
+
+})*/
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,27 @@
-const mdLinks = require('../');
+//const { mdLinks } = require('../index.js').default;
+
+import { mdLinks } from "../index.js"
+
+
+
+describe('mdLinks', () => {
+  /*it('should...', () => {
+    console.log('FIX ME!');
+  });
+  // es una promesa mdLinks ¿?
+  it('Debería devolver una promesa', () => {
+    expect(mdLinks()).toBe(typeof Promise);
+  });*/
+  //Existe un path¿? (probamos cuando no existe un path)
+  it('No existe esta ruta', () => {
+    return mdLinks('/noExistente.md').catch((error) => {
+    expect(error).toBe('Esta ruta no existe')  
+    })
+  });
+});
+
+
+/*const mdLinks = require('../');
 
 
 describe('mdLinks', () => {
@@ -7,4 +30,4 @@ describe('mdLinks', () => {
     console.log('FIX ME!');
   });
 
-});
+});*/
