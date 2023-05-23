ac84b228a4712c7cce147b68f938375a6379aa9f
Maria Garcia <licgarcia_psic@hotmail.com>
2023-05-23 13:07:57 -0600

    Extraer Links

--- api.js
+++ api.js
@@ -1,40 +1,49 @@
-import { readFile } from 'fs';
+import { clear } from "console";
+import { readFile } from "fs";
+import path, { resolve } from "path";
 
-export const readMD = (path = "README.md") => {
-  return new Promise((resolve, reject) => {   
-//5. LEEMOS EL ARCHIVO 
-readFile(path, 'utf8',  function(err, data) {
-if (err) {
-  return reject (err);
-}
+export const readMD = (path) => {
+  //mi parametro neesita un argumento....prgress sera el argumento investiga... por lo general si necesita el argumento solo hay casos especificos que no lo necesita
+  // 1node 2 archivo 3 readme. 4 --validate
+  return new Promise((resolve, reject) => {
+    //5. LEEMOS EL ARCHIVO
+    readFile(path, "utf8", function (err, data) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(data);
+    });
+  });
+};
 
-return resolve(data);
-});
-  })
-}
+export const findLinks = (contenido) => {
+console.log(contenido);
+    // const arrayLinks = [];
+    //5. LEEMOS LINKS
+    let regex = /\[([^\]!]+)]\((https:\/\/[^\)]+)\)/gi;
 
-export const findLinks = () => {
+    // const myMatch = string.match(regex)
+    // const [ text, url ] = myMatch
+    // console.log(text)
+    // console.log(url)
 
-//5. LEEMOS LINKS
-let string = `
-* [Arreglos](https://curriculum.laboratoria.la/es/topics/javascript/04-arrays)
-* [Array - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/)
-* [Array.prototype.sort() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
-* [Array.prototype.forEach() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
-* [Array.prototype.map() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
-* [Array.prototype.filter() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter)
-* [Array.prototype.reduce() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce)`
-
-let regex = /\[([^\]!]+)]\((https:\/\/[^\)]+)\)/gi
-const myMatch = string.match(regex)
-console.log(myMatch)
-// const [ {full, text, url} ] = myMatch
-// console.log(text)
-// console.log(url)
+    const myMatch = [...contenido.matchAll(regex)].map((m) => ({
+      text: m[1],
+      url: m[2],
+    }));
+    return myMatch;
   }
 
 // mdLinks('C:/Users/HP-1/Desktop/MariaGracia/Proyectos MariaGracia/MD-links/DEV004-md-links/README.md'); absoluta
 //readMD().then( (res) => console.log(res)).catch((err) => console.log(err)); //Tengo ue tomar la respuesta de MD
-findLinks(readMD())//Tengo ue tomar la respuesta de MD
+// let ejemploLinks = `
+// * [Arreglos](https://curriculum.laboratoria.la/es/topics/javascript/04-arrays)
+// * [Array - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/)
+// * [Array.prototype.sort() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
+// * [Array.prototype.forEach() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
+// * [Array.prototype.map() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
+// * [Array.prototype.filter() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter)
+// * [Array.prototype.reduce() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce)`
 
-
+// console.log(findLinks(ejemploLinks))
+// logOut().then((resp) => onNavigate("/"));
--- index.js
+++ index.js
@@ -1,51 +1,41 @@
 import { existsSync, statSync } from "fs";
 //fs módilo que permite interactuar con archivos del sistema
 import { isAbsolute, resolve as resolvePath, extname } from "path";
-import { readMD } from "./api.js";
-import { runInThisContext } from "vm";
+import { findLinks, readMD } from "./api.js";
 
 //'babel.config.json'
 //'README.md'
 //'test'
 //'mari/README.md'
 //'C:/Users/HP-1/Desktop/MariaGracia/Proyectos MariaGracia/MD-links/DEV004-md-links/jest.config.js'
-export const mdLinks = (path = "README.md", options) => {
+
+export const mdLinks = (ruta) => {
+  //investiga process.argv (detecta lo que vas a escribir en consola) en el CLI tenlo presente
   return new Promise((resolve, rejects) => {
-    const isPath = existsSync(path);
-    const isPathAbsolute = isAbsolute(path);
-    let stats = statSync(path);
+    const isPath = existsSync(ruta);
+    let stats = statSync(ruta);
     const file = stats.isFile();
-    const typeFile = extname(path);
-    //1. IDENTIFICA SI EXISTE UNA RUTA
+    const typeFile = extname(ruta);
+    //investigar extname....este path es del parametro que uso en mi funcion(ruta)... el primero es parte de una función
+    //TODO: 1. IDENTIFICA SI EXISTE UNA RUTA
     if (isPath === true) {
       console.log("RUTA EXISTENTE");
       //2. LA RUTA ES ABSOLUTA ¿? Extra (si es un archivo o directorio)
-      if (isPathAbsolute === true) {
-        console.log("RUTA ABSOLUTA: ", path);
-        //3. ES UNA ARCHIVO ¿?
-        if (file === true) {
-          console.log("ARCHIVO: ", extname(path));
-          //4. ES UN ARCHIVO MD ¿?
-          if (typeFile === ".md") {
-            readMD()
-          }else{
-            console.log("Por el momento sólo acepta archivos .md");
-          }
+      if (!isAbsolute(ruta)) {
+        ruta = resolvePath(ruta);
+      }
+      //3. ES UNA ARCHIVO ¿?
+      if (file === true) {
+        console.log("ARCHIVO: ", typeFile);
+        //4. ES UN ARCHIVO MD ¿?
+        if (typeFile === ".md") {
+          readMD(ruta).then(contenido => resolve(findLinks(contenido)));
+          // console.log(readMD(ruta).then( (res) => console.log(res)).catch((err) => console.log(err)));
         } else {
-          console.log("Por el momento sólo leemos archivos");
+          console.log("Por el momento sólo acepta archivos .md");
         }
       } else {
-        console.log("CONVERSIÓN A RUTA ABSOLUTA:  ", resolvePath(path));
-        if (file === true) {
-          console.table("ARCHIVO: ", extname(path));
-          if (typeFile === ".md") {
-            readMD()
-          }else{
-            console.log("Por el momento sólo acepta archivos .md");
-          }
-        } else {
-          console.log("Sólo pueden leerse archivos");
-        }
+        console.log("Por el momento sólo leemos archivos");
       }
     } else {
       console.log("RUTA NO EXISTENTE");
@@ -53,4 +43,4 @@ export const mdLinks = (path = "README.md", options) => {
     }
   });
 };
-mdLinks();
+mdLinks('README.md').then(console.log);
