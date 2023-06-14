c83bd2511ba74b8a72653a6ce800b3efe9385887
Maria Garcia <licgarcia_psic@hotmail.com>
2023-06-13 23:48:42 -0600

    Funcionalidad concluída

--- cli.js
+++ cli.js
@@ -2,39 +2,39 @@ import { argv } from "node:process";
 import { mdLinks } from "./index.js";
 
 export const optionsCli = () => {
-const isValidate = argv.includes("--validate");
-const isStats = argv.includes("--stats");
-const isFirstOption = process.argv[3] === '--stats' && process.argv[4] === '--validate';
-const isSecondOption = process.argv[3] === '--validate' && process.argv[4] === '--stats';
-const isBreakLinks = isFirstOption || isSecondOption
-const ruta =  argv[2];
-const options = { validate:isValidate, stats:isStats, combo:isBreakLinks };
-mdLinks(ruta, options).then((res)=>{
+  const isValidate = argv.includes("--validate");
+  const isStats = argv.includes("--stats");
+  const isFirstOption =
+    process.argv[3] === "--stats" || process.argv[3] === "--validate";
+  const isSecondOption =
+    process.argv[4] === "--validate" || process.argv[4] === "--stats";
+  const isBreakLinks = isFirstOption && isSecondOption;
+  const ruta = argv[2];
+  const options = { validate: isValidate, stats: isStats, combo: isBreakLinks };
+  mdLinks(ruta, options).then((res) => {
     const totalLiks = res.length;
     const uniqueLinks = [...new Set(res.map((link) => link.href))].length;
     const breakLinks = res.filter((res) => res.status >= 400).length;
-if (isValidate && !isStats) {
-    console.log(res)
-  } 
-  else if (isStats && !isValidate) {
-    console.log('Total:', totalLiks)
-    console.log('Unique:',uniqueLinks);
-  }
-  else if (options.combo) {
-    console.log('Total:', totalLiks)
-    console.log('Unique:',uniqueLinks);
-    console.log('Broken:', breakLinks);
-  }
-})
-}
-optionsCli()
+    if (isValidate && !isStats) {
+      console.log(res);
+    } else if (isStats && !isValidate) {
+      console.log(`${"Total: " + totalLiks}\n${"Unique: " + uniqueLinks}`);
+    } else if (options.combo) {
+      console.log(
+        `${"Total: " + totalLiks}\n${"Unique: " + uniqueLinks}\n${
+          "Broken: " + breakLinks
+        }`
+      );
+    }else
+    {
+      console.log(
+        `${"Opciones para revisar links y estadísticas de archivos .md:"}\n${
+          "--validate (conocer links)"}\n${"--stats (Total de links y unicos)"}\n${
+          "--validate --stats o --stats --validate (links archivo, links únicos y links rotos)"}`
+      );
+    }
+  });
+};
+optionsCli();
 
-//implementar las opciones, que va a pasar con la primera opcion ejemplo --validate... o que pasaria si
-//no habría una option 
-//que pasa si no estuviera escrito alidate 
-//links repetidos por href... como elimitar los repetidos ... busca:new Set
-//..  reduce...suma los links .... legth no tienes que sumarlo..... nomrbro unique y le paso el valor del length de links 
-// options = {validate:true, stats:true}...........
-// funcion que recibe un arreglo de links y que su output => un nuevo arreglo pero solamente con links no repetidos ... recomendacion hacer en API pir cuestion de orden 
- // ahora dejalo... porque aún no tomo el valor .md hasta que lo captures ya no es necesario por ahora no lo estoy haciendo 
 //versión de chalk chalk 4.1.2 npm install react@16.14.0
